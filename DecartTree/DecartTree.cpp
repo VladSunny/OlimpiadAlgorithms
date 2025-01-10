4061dbdb89e9fe244bf85ec36dbdc4c2470fbd28
Vlad Sunny <vladislavsunny@gmail.com>
2025-01-10 18:38:14 +0300

    DecartTree
    Realization

---
+++ DecartTree/DecartTree.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+mt19937 rng(52);
+
+struct Node {
+    int key, priority;
+    Node *left, *right;
+
+    Node(int key) {
+        this->key = key;
+        priority = rng();
+        left = right = nullptr;
+    }
+};
+
+pair<Node*, Node*> split(Node* v, int x) {
+    if (v == nullptr) return {nullptr, nullptr};
+
+    if (x >= v->key) {
+        auto [r_new, r] = split(v->right, x);
+        v->right = r_new;
+        return {v, r};
+    } else {
+        auto [l, l_new] = split(v->left, x);
+        v->left = l_new;
+        return {l, v};
+    }
+}
+
+Node* merge(Node* l, Node* r) {
+    if (l == nullptr) return r;
+    if (r == nullptr) return l;
+
+    if (l->priority > r->priority) {
+        Node* r_new = merge(l->right, r);
+        l->right = r_new;
+        return l;
+    } else {
+        Node* l_new = merge(l, r->left);
+        r->left = l_new;
+        return r;
+    }
+}
+
+Node* insert(Node* v, int x) {
+    auto [l, r] = split(v, x);
+    return merge(merge(l, new Node(x)), r);
+}
+
+Node* erase(Node* v, int x) {
+    auto [l, r] = split(v, x);
+    auto [m, r_new] = split(r, x + 1);
+    return merge(l, r_new);
+}
+
+int main() {
+
+    return 0;
+}
+
